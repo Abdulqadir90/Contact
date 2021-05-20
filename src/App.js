d99d0ed4c879f9dbe50c7755fdc44322f1adf9d6
Abdulqadir Abdillahi <33445223+Abdulqadir90@users.noreply.github.com>
2021-05-20 21:35:37 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import contacts from "./data.js";
+
+
+function App() {
+    console.log(contacts);
+    return (
+        <div className="App">
+            <h1>My address book</h1>
+        </div>
+    );
+}
+
+export default App;
